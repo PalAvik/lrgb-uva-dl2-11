27dd6d3a61feadfd89e1372ba40d23bb8234e828
madhurapawaruva <117770386+madhurapawaruva@users.noreply.github.com>
2023-05-19 09:52:05 +0200

    added support for egnn

--- analysis/noising_experiments/noise_utils.py
+++ analysis/noising_experiments/noise_utils.py
@@ -6,13 +6,20 @@ from torch_geometric.graphgym.config import (cfg, dump_cfg,
                                              )
 
 def get_predictions(data, model):
-    if cfg.model.type == 'enn':
+    if cfg.model.type in ['enn', 'egnn']:
+            data = data.to(torch.device(cfg.device))
             nodes = data["x"][:,:12].to(torch.device(cfg.device))
             positions = data["x"][:,12:].to(torch.device(cfg.device))
             edges = data["edge_index"].to(torch.device(cfg.device))
             edge_attr = data["edge_attr"].to(torch.device(cfg.device))
             true = data["y"]
-            pred = model(h0=nodes, x=positions, edges=edges, edge_attr=edge_attr)
+            if cfg.model.type == "enn":
+               pred = model(h0=nodes, x=positions, edges=edges, edge_attr=edge_attr)
+            else:
+                n_nodes_arr = [data.x.size(0)]
+                tensor_n_nodes = torch.tensor(n_nodes_arr)
+                tensor_n_nodes_interleaved = tensor_n_nodes.repeat_interleave(tensor_n_nodes).unsqueeze(1).to(torch.device(cfg.device))
+                pred = model(h0=nodes, x=positions, edges=edges, edge_attr=edge_attr, x_weights=tensor_n_nodes_interleaved)
     else:
         data = data.to(torch.device(cfg.device))
         pred, true = model(data)
