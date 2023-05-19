Jacobian fix for egnn

graphgps/custom/egnn/custom_egnn.py
@@ -118,7 +118,7 @@ class E_GCL_mask2(E_GCL):
         self.act_fn = act_fn
 
     def coord_model(self, coord, edge_index, coord_diff, edge_feat, x_weights):
-        row, col = edge_index
+        row, col = edge_index.to(torch.int64)
         trans = coord_diff * self.coord_mlp(edge_feat) 
         agg = unsorted_segment_sum(trans, row, num_segments=coord.size(0))
         coord_update = torch.div(agg, x_weights)

model_inference.py
@@ -155,8 +155,13 @@ if __name__ == '__main__':
                     edges.requires_grad_(False)
                     edge_attr.requires_grad_(True)
 
-                    if cfg.model.type in ['enn', 'egnn']:
+                    if cfg.model.type == 'enn':
                         input_ = (nodes, positions, edges, edge_attr)
+                    elif cfg.model.type == 'egnn':
+                        n_nodes_arr = [graph.x.size(0)]
+                        tensor_n_nodes = torch.tensor(n_nodes_arr)
+                        t_n = tensor_n_nodes.repeat_interleave(tensor_n_nodes).unsqueeze(1).to(torch.device(cfg.device))
+                        input_ = (nodes, positions, edges, edge_attr, t_n.float())
                     elif cfg.model.type == 'GPSModel':
                         EigVals = graph.EigVals.to(torch.device(cfg.device))
                         EigVecs = graph.EigVecs.to(torch.device(cfg.device))